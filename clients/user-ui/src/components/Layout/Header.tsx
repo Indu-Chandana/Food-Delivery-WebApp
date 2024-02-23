66a60aefc8bc05a5af83746a59f0265adef8d24b
Indu Chandana <induchandana@Indus-MacBook-Pro.local>
2024-02-23 11:58:10 +0530

    Feb23 001 setUp -> nextUi, font(Poppins), folderStructure

---
+++ clients/user-ui/src/components/Layout/Header.tsx
@@ -0,0 +1,18 @@
+// import { Avatar } from '@nextui-org/react'// This work for client component
+import styles from '@/src/utils/style'
+import NavItems from '../NavItems'
+import ProfileDropDown from "../ProfileDropDown"
+
+const Header = () => {
+    return (
+        <header className='w-full bg-[#0F1524]'>
+            <div className=' w-[90%]  h-[80px]  m-auto flex items-center justify-between'>
+                <h1 className={`${styles.logo}`}>Foodie</h1>
+                <NavItems />
+                <ProfileDropDown />
+            </div>
+        </header>
+    )
+}
+
+export default Header
